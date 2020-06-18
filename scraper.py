b7699c34a23b4e492195a1441f80dd92d66973b0
danadarvishzadeh <danadarvishzadeh25@gmail.com>
2020-06-19 01:58:53 +0430

    making scraper

--- scraper.py
+++ scraper.py
@@ -6,6 +6,19 @@ this script will check each word's correctness
 from an online dictionary
 """
 
-word_list = []
+word_list = ['تاذل', 'لب', 'اقا', 'لفب', 'بسیی', 'کباب', 'محمد']
 
-url = 
+
+with requests.Session() as s:
+    for word in word_list:
+        url = 'http://www.vajehyab.com/?q=' + word + '&f=dehkhoda'
+        response = s.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        answer = soup.find('div', attrs={'class':'fieldset'})
+        if answer:
+            if answer.text.strip() == 'جست‌وجوی دقیق':
+                print(word, 'is word')
+            else:
+                print(word, 'not a word')
+        else:
+            print(word, 'not a word')
