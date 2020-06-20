b6656e6530d948e4812a5714e9c961d0855c2047
danadarvishzadeh <danadarvishzadeh25@gmail.com>
2020-06-20 14:52:45 +0430

    final scraper and wordchecker

--- scraper.py
+++ scraper.py
@@ -1,29 +1,50 @@
+import re
 import requests
 from bs4 import BeautifulSoup
-import concurrent.futures
+from concurrent.futures import as_completed, ThreadPoolExecutor
 import threading
+from requests_futures.sessions import FuturesSession
 """
 this script will check each word's correctness
 from an online dictionary
 """
 
-word_list = []
+inputs = input().strip().split(' ')
 
-print_lock = threading.Lock()
+word_list = WordMaker(len(inputs), inputs)
 
-def WordCheck(word):
-    url = 'http://www.vajehyab.com/?q=' + word + '&f=dehkhoda'
-    response = requests.get(url)
+answers = []
+
+append_lock = threading.Lock()
+
+
+def Appender(val, target):
+    with append_lock:
+        target.append(val)
+
+def Vajeh_Check(response, word):
     soup = BeautifulSoup(response.text, 'html.parser')
-    answer = soup.find('div', attrs={'class':'fieldset'})
-    with print_lock:
-        if answer:
-            if answer.text.strip() == 'جست‌وجوی دقیق':
-                print(word, 'is word')
-            else:
-                print(word, 'not a word')
+    check_result = soup.find('div', attrs={'class':'fieldset'})
+    if check_result:
+        if check_result.text.strip() == 'جست‌وجوی دقیق':
+            Appender(word, answers)
         else:
-            print(word, 'not a word')
+            pass
+    else:
+        pass
 
-with concurrent.futures.ThreadPoolExecutor() as executor:
-    executor.map(WordCheck, word_list)
+    
+start = datetime.datetime.now()
+
+with FuturesSession(max_workers=2) as session:
+    futures = [session.get('http://www.vajehyab.com/?q=' + word + '&f=dehkhoda')
+              for word in word_list]
+    prints = 0
+    for future, word in zip(as_completed(futures), word_list):
+        response = future.result()
+        Vajeh_Check(response, word)
+        prints += 1
+    print(len(word_list), prints)
+        
+end = datetime.datetime.now()
+print(end-start)
